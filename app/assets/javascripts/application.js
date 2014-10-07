3e11ed91cb22988b85a6e1757e0b6ec8daad63ab
fgarbagnati <f.g.garbagnati@gmail.com>
2014-10-06 22:35:07 -0700

    scaffolded some backbone stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/app
 //= require_tree .
 //= require bootstrap.min
